496870cb71b7489e0702250509ffb5f807a23403
palucena <palucena@student.42malaga.com>
2023-10-25 12:51:00 +0200

    name

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: palucena <palucena@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 19:07:37 by palucena          #+#    #+#             */
-/*   Updated: 2023/10/25 00:31:18 by palucena         ###   ########.fr       */
+/*   Updated: 2023/10/25 12:49:45 by palucena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,17 @@
 int	main(int argc, char **argv, char **envp)
 {
 	char	*input;
+	char	*name;
 	t_lx	*lex;
 
 	(void)argc;
 	(void)argv;
 	(void)envp; // este no
 	//ft_env(envp);
+	name = "minishell";
 	while (1)
 	{
-		input = readline("\033[36;1mminishell$ \033[0m");
+		input = readline(ft_strjoin(ft_strjoin("\033[36;1m", name), "$ \033[0m"));
 		if (!input || ft_strcmp(input, "exit"))
 			break ;
 		if (!ft_strcmp(input, "\0"))
