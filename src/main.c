c62ad528acac92a1398c06f16fc99662a3a39f90
rdelicad <rdelicad@student.42malaga.com>
2023-10-31 14:36:29 +0100

    creado promt minishell

--- src/main.c
+++ src/main.c
@@ -6,11 +6,17 @@
 /*   By: rdelicad <rdelicad@student.42.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 19:15:28 by rdelicad          #+#    #+#             */
-/*   Updated: 2023/10/30 15:06:12 by rdelicad         ###   ########.fr       */
+/*   Updated: 2023/10/31 14:34:45 by rdelicad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/env.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
 
 void	init_struct(t_cmd *c)
 {
@@ -58,7 +64,7 @@ void	create_list_env(t_cmd *c, char **envp, int len_envp)
 	}
 }
 
-int	main(int ac, char **av, char **envp)
+/* int	main(int ac, char **av, char **envp)
 {
 	t_cmd	c;
 
@@ -82,4 +88,56 @@ int	main(int ac, char **av, char **envp)
 	ft_free_list(c.list_env);
 	//atexit(leaks);
 	return (0);
+} */
+#define MAX_COMMAND_LENGTH 100
+
+int main(int ac, char** av, char** envp) 
+{
+    t_cmd c;
+	(void)ac;
+	(void)av;
+    init_struct(&c);
+    create_list_env(&c, envp, len_envp(envp));
+
+    char command[MAX_COMMAND_LENGTH];
+    char* args[MAX_COMMAND_LENGTH];
+
+    while (1) {
+        printf("minishell> ");
+        fgets(command, MAX_COMMAND_LENGTH, stdin);
+
+        // Eliminar el salto de línea final
+        command[strcspn(command, "\n")] = 0;
+
+        // Dividir el comando en argumentos
+        char* token = strtok(command, " ");
+        int i = 0;
+        while (token != NULL) {
+            args[i] = token;
+            token = strtok(NULL, " ");
+            i++;
+        }
+        args[i] = NULL;
+
+        // Salir de la minishell si se ingresa "exit"
+        if (strcmp(args[0], "exit") == 0) {
+            break;
+        }
+
+        // Verificar los comandos ingresados y llamar a las funciones correspondientes
+        if (!strcmp(args[0], "env")) {
+            ft_env(&c);
+        } else if (!strcmp(args[0], "export")) {
+            if (i > 1) {
+                c.argv_env = args[1];
+            }
+            ft_export(&c);
+        } else {
+            ft_printf("Comando inválido.\n");
+        }
+    }
+
+    ft_free_list(c.list_env);
+    //atexit(leaks);
+    return 0;
 }
