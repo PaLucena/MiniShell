2f13c21a880143a4ee49ef8a0df5bcfbe4937d07
rdelicad <rdelicad@student.42malaga.com>
2023-11-06 20:46:43 +0100

    comprobar leaks en cd y cd sin argumentos

--- src/builtins.c
+++ src/builtins.c
@@ -6,7 +6,7 @@
 /*   By: rdelicad <rdelicad@student.42.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 13:25:14 by rdelicad          #+#    #+#             */
-/*   Updated: 2023/11/06 20:28:02 by rdelicad         ###   ########.fr       */
+/*   Updated: 2023/11/06 20:46:00 by rdelicad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,7 @@
 void	ft_builtins(t_cmd *c, char **args, int i)
 {
 	if (strcmp(args[0], "env") == 0) 
-	{
-		if (i > 1)
-		{
-			ft_putstr_fd("Error, no se pueden introducir mas argumentos", 2);
-			return ;
-		}
 		ft_env(c);
-	}
 	else if (!strcmp(args[0], "export")) 
 	{
 		if (i > 1)
